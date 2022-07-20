8a4f88f517a515e070a96be70e26d8250f74a26b
Mukul Jataria <mukuljataria0723@gmail.com>
2022-07-20 13:22:20 +0530

    Setup Done

---
+++ src/store/index.js
@@ -0,0 +1,12 @@
+import { applyMiddleware, combineReducers, createStore } from 'redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+import reducer from '../reducers/index';
+
+let store;
+
+export function configureStore() {
+  store = createStore(reducer, applyMiddleware(thunk));
+
+  return store;
+}
